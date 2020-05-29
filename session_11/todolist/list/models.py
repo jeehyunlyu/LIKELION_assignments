18eaf994abf0e933e2ffe381e1adba38c62383d6
lyujeehyun <jeehyun9808@naver.com>
2020-05-29 21:38:44 +0900

    session11 assignment

---
+++ session_11/todolist/list/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class ToDo(models.Model):
+    title = models.CharField(max_length=20)
+    content = models.TextField()
+    duedate = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    todo = models.ForeignKey(ToDo, on_delete=models.CASCADE, related_name="comments")
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+
+    def __str__(self):
+        return self.content
